f390ec534860b15d514f05335a248c7ec83fcf5d
felixmccuaig <felixmccuaig@gmail.com>
2024-12-19 11:12:01 +1100

    Fix openai in test

--- api/src/config/openAI.ts
+++ api/src/config/openAI.ts
@@ -4,7 +4,7 @@ import OpenAI from "openai";
 
 export var openai: any;
 
-if (process.env.NODE_ENV === "test") {
+if (process.env.NODE_ENV !== "test") {
   openai = new OpenAI({
     apiKey: process.env["OPENAI_API_KEY"],
   });
